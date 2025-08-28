4a6f4d5f8b1d0dce952b727d02969a06a7709f6e
Jonah Colestock <rcolest@clemson.edu>
2025-08-28 19:12:39 -0400

    Created skeleton for main function
    Vaguely based on pseudocode

--- StudyBuddy.java
+++ StudyBuddy.java
@@ -4,26 +4,57 @@ public class StudyBuddy{
 
     public static void main(String[] args){
         System.out.println("Welcome to the Clemson Study Buddy Helper App!\n");
+        System.out.println("Input your selection:\n1. Create Profile\n2.View Profile\n3.Add Availability\n4.Remove Availability\n5.Search for Classmates\n6.Suggest Matches\n7.Schedule Session\n8.Confirm Session\n9.Exit");
+        int num = System.in.read();
+        switch(num){
+            case 1:
+                createProfile();
+                break;
+            case 2:
+                //print profile information
+                break;
+            case 3:
+                //add availability
+                break;
+            case 4:
+                //remove availability
+                break;
+            case 5:
+                //search for classmates
+                break;
+            case 6:
+                //suggest matches
+                break;
+            case 7:
+                //schedule session
+                break;
+            case 8:
+                //confirm session
+                break;
+            case 9:
+                //exit
+                break;
+        }
         
     }
 
-    createProfile(){
+    public void createProfile(){
 
     }
 
-    searchByCourse(){
+    public void searchByCourse(){
 
     }
 
-    suggestMatches(){
+    public void suggestMatches(){
 
     }
 
-    scheduleSession(){
+    public void scheduleSession(){
 
     }
 
-    confirmSession(){
+    public void confirmSession(){
 
     }
 }
