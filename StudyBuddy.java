5a49553c28e5201c5f51a441a0cc550410970b67
Jonah Colestock <rcolest@clemson.edu>
2025-08-28 18:58:26 -0400

    Created StudyBuddy.java
    Based solely on UML class diagram

---
+++ StudyBuddy.java
@@ -0,0 +1,29 @@
+public class StudyBuddy{
+    ArrayList<Student> students;
+    ArrayList<StudySession> sessions;
+
+    public static void main(String[] args){
+        System.out.println("Welcome to the Clemson Study Buddy Helper App!\n");
+        
+    }
+
+    createProfile(){
+
+    }
+
+    searchByCourse(){
+
+    }
+
+    suggestMatches(){
+
+    }
+
+    scheduleSession(){
+
+    }
+
+    confirmSession(){
+
+    }
+}
