5fe0b3d07731e41b614ce020aae62582dea418cc
Jonah Colestock <rcolest@clemson.edu>
2025-08-28 18:52:37 -0400

    Created StudySession.java
    Based solely on UML class diagram

---
+++ StudySession.java
@@ -0,0 +1,16 @@
+public class StuddySession{
+    public static void main(String[] args){
+        ArrayList<Student> participants;
+        String course;
+        TimeSlot timeSlot;
+        String status; //proposed, confirmed
+    }
+
+    public void propose(){
+        status = "proposed";
+    }
+
+    public void confirm(){
+        status = "confirmed";
+    }
+}
