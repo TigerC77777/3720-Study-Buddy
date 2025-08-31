137d52b3b0d9270d5ac34dfe66e1d5b1fbb91287
Jonah Colestock <rcolest@clemson.edu>
2025-08-30 22:36:27 -0400

    Changes to first test

--- test/StudyBuddyTest.java
+++ test/StudyBuddyTest.java
@@ -3,6 +3,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,18 +14,11 @@ class StudyBuddyTest {
     }
 
     @org.junit.jupiter.api.Test
-    void testCreateProfileAndPrintProfile() {
-        String name = "Jonah Colestock";
-        ArrayList<String> courses = new ArrayList<String>();
-        courses.add("CPSC 3720");
-        courses.add("CPSC 4300");
-        courses.add("CPSC 2310");
-        String simulatedInput = "Jonah\nColestock\n1\nCPSC\n3720\n1\nCPSC\n4300\nCPSC\n2310\n2\n1\nOctober 3\n0830\n1230\n2\n";
-        InputStream originalIn = System.in;
-        InputStream originalOut = System.out;
-        System.setIn(new ByteArrayInputStream(simulatedInput.getBytes()));
-        ByteArrayOutputStream out = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(out));
+    void testCreateProfileAndPrintProfile(){
+        Scanner fakeScanner = new Scanner("Jonah\nColestock\n1\nCPSC\n3720\n1\nCPSC\n4300\nCPSC\n2310\n2\n1\nOctober 3\n0830\n1230\n2\n");
+        StudyBuddy app = new StudyBuddy();
+        app.createProfile(fakeScanner);
+        
     }
 
 
