20a96169c8ef8631af4f4969ab62b4f6ea16e461
Jonah Colestock <rcolest@clemson.edu>
2025-08-31 01:53:49 -0400

    More tests and fixes

--- test/StudentTest.java
+++ test/StudentTest.java
@@ -11,14 +11,30 @@ class StudentTest {
     @Test
     void addCourseTest() {
         Scanner fakeScanner = new Scanner("CPSC\n4300\n");
-        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-        PrintStream originalOut = System.out;
-        System.setOut(new PrintStream(outContent));
         Student me = new Student("Jonah");
         me.AddCourse(fakeScanner);
-        System.setOut(originalOut);
         assertFalse(me.getCourses().isEmpty());
         String myClass = me.getCourses().get(0);
         assertEquals("CPSC 4300", myClass);
+    }
+
+    @Test
+    void addAvailabilityTest(){
+        Scanner fakeScanner = new Scanner("October 3\n1230\n1630\n");
+        Student me = new Student("Jonah");
+        me.AddAvailability(fakeScanner);
+        assertFalse(me.getAvailability().isEmpty());
+        TimeSlot time = new TimeSlot("October 3", 1230, 1630);
+        assertTrue(me.getAvailability().get(0).equals(time));
+    }
+
+    @Test
+    void removeAvailabilityTest(){
+        Scanner fakeScanner = new Scanner("October 3\n1230\n1630\n");
+        Student me = new Student("Jonah");
+        me.AddAvailability(fakeScanner);
+        Scanner fakeScanner2 = new Scanner("October 3\n1230\n1630\n");
+        me.RemoveAvailability(fakeScanner2);
+        assertTrue(me.getAvailability().isEmpty());
     }
 }
--- test/StudyBuddyTest.java
+++ test/StudyBuddyTest.java
@@ -29,5 +29,18 @@ class StudyBuddyTest {
         assertTrue(output.contains("October 3, 830 to 1230"));
     }
 
+    @Test
+    void testSearchByCourse(){
+        Scanner fakeScanner = new Scanner("Jonah\nColestock\n1\nCPSC\n3720\n1\nCPSC\n4300\n1\nCPSC\n2310\n2\n1\nOctober 3\n0830\n1230\n2\n");
+        StudyBuddy.createProfile(fakeScanner);
+        Scanner fakeScanner2 = new Scanner("Jack\nHilliard\n1\nCPSC\n4300\n1\nCPSC\n2310\n2\n1\nOctober 3\n0830\n1230\n2\n");
+        StudyBuddy.createProfile(fakeScanner2);
+        ArrayList<Student> us = StudyBuddy.searchByCourse("CPSC 2310");
+        assertFalse(us.isEmpty());
+        for(Student s : us){
+            assertTrue(s.getName().equals("Jonah Colestock") || s.getName().equals("Jack Hilliard"));
+        }
+    }
+
 
 }
